4e025b89beeb072a7bf75e0471207c803469d83f
Hyunho Lee <mizzking75@gmail.com>
2020-02-10 23:47:40 +0900

    shopping detail

--- shop/views.py
+++ shop/views.py
@@ -16,11 +16,12 @@ def product_in_category(request, category_slug=None):
                   {'current_category': current_category, 'categories': categories, 'products': products})
 
 
-# from cart.forms import AddProductForm
-#
-#
+#from cart.forms import AddProductForm
+
+
 def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id, slug=product_slug)
     #add_to_cart = AddProductForm(initial={'quantity': 1})
-    add_to_cart = 1
-    return render(request, 'shop/detail.html', {'product': product, 'add_to_cart': add_to_cart})
+    #add_to_cart = 1
+    #, 'add_to_cart': add_to_cart
+    return render(request, 'shop/detail.html', {'product': product})
